3e6d29f034dfe3b03d226ee7f0455ad4e226097a
Johan Guldmyr <johan.guldmyr@csc.fi>
2016-02-26 07:53:45 +0200

    fix travis

--- tests/launch-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build --rm -t local/c7-systemd -f tests/Dockerfile .
-sudo docker run -it --privileged=true --rm=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v `pwd`:/ansible-role-pxe_config:rw local/c7-systemd /bin/bash -c "/ansible-role-pxe_config/tests/test-in-docker-image.sh ${OS_TYPE} ${OS_VERSION} ${ANSIBLE_VERSION}"
